ef417cc3fd8102eae9e83a4690d23acaa9a83af6
ievgen <jekripper@gmail.com>
2021-12-26 21:52:44 +0200

    12

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "cv#index"
+  root "cv#index", as: "home"
   get 'cv/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
